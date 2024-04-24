ef5f1fb8bc013afac69b87ef4166dd4e197e295c
neerajan rai <141233559+SayianPi@users.noreply.github.com>
2024-04-24 16:19:09 +0545

    added search filter with name and roll in class 2

--- SMS/Pages/Classroom/ClassTwo/Index.cshtml.cs
+++ SMS/Pages/Classroom/ClassTwo/Index.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SMS.Data;
 using SMS.Model;
 using SMS.Model.Class2;
+using System.Linq;
 
 namespace SMS.Pages.Classroom.ClassTwo
 {
@@ -12,20 +14,54 @@ namespace SMS.Pages.Classroom.ClassTwo
     {
         private readonly ApplicationDbContext _context;
         public IEnumerable<Class2> Classtwo { get; set; }
-        public IList<Class2> FirstStudent { get; set; }
-        
+        public IEnumerable<Class2> Class2 { get; set; }
+
         public IndexModel(ApplicationDbContext _db)
         {
             _context = _db;
         }
-        public void OnGet()
+
+        //FOR SEARCH
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? RollNo { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? StudentRollno { get; set; }
+        //
+        //public void OnGet()
+        //{
+        //    Classtwo = _context.ClassTwo;
+        //    //var FirstStudent = _context.ClassTwo.FromSqlRaw("Select * from ClassTwo where Rollno = '1'");
+        //    var FirstStudent = Classtwo.Where(u => u.Rollno.Equals(1));
+        //}
+        public async Task OnGetAsync()
         {
+            Class2 = _context.ClassTwo;
             Classtwo = _context.ClassTwo;
             //var FirstStudent = _context.ClassTwo.FromSqlRaw("Select * from ClassTwo where Rollno = '1'");
             var FirstStudent = Classtwo.Where(u => u.Rollno.Equals(1));
 
+            //FOR SEARCH
+            var students = from m in _context.ClassTwo
+                         select m;
+            IQueryable<int> rollQuery = from m in _context.ClassTwo
+                                        orderby m.Rollno
+                                        select m.Rollno;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                students = students.Where(s => s.FullName.Contains(SearchString));
+            }
+            //if (!int.IsNullOrEmpty(StudentRollno))
+            if(StudentRollno != null)
+            {
+                students = students.Where(x => x.Rollno.Equals(StudentRollno));
+            }
 
+            Classtwo = await students.ToListAsync();
+            RollNo = new SelectList(await rollQuery.Distinct().ToListAsync());
+            //
         }
-        
+
     }
 }
